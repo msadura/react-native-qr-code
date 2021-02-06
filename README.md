4dd358a3004b6e80c525592b093d7c007f7e98b9
Marek Sadura <m.sadura@outlook.com>
2021-02-06 10:58:20 +0100

    readme, lib code

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # react-native-qr-code
-React Native component for generating and displaying qr code
+React Native component for generating and displaying qr codes. Uses js library [node-qrcode](https://github.com/soldair/node-qrcode) for generating qr code data. Uses pure react-native solution to display generated code, does not need additional dependencies to render code on screen.
+
+![](Example/btn_anim.gif)
+
+# Saving code as image
+Library will use [react-native-view-shot](https://github.com/gre/react-native-view-shot) to save generated code as image. If you need this feature you should install and link this library in your project.
+
+NOTE: This is needed only if you want to save code as image, code can be generated without this dependency.
+
+# Usage
+Install module:
+```
+npm install --save react-native-qr-code
+```
+
+or
+
+```
+yarn add react-native-qr-code
+```
+
+Import component and use it in your app:
+```js
+import QRCode from 'react-native-qr-code';
+
+export default function App() {
+  const qrRef = useRef();
+
+  const saveQrAsImg = async () => {
+    const uri = qrRef.current.saveImg();
+    console.log('QR code img uri: ', uri);
+  }
+
+  return (
+      <View>
+        <QRCode
+          value="https://github.com/msadura/react-native-qr-code"
+          size={200}
+          ref={qrRef}
+        />
+      </View>
+  );
+}
+```
+
+# CyberButton props
+| Props                | Type          | Description  | Default      |
+| --------------------- |:-------------:| ------------ | ------------ |
+| `value`  | `string` | Value of code to be generated.| `''` |
+| `size` | `Integer`     | Used as height andf width of generated QR code. |`200` |
+| `color`  | `string` | Qr code main color.| `black` |
+| `backgroundColor`  | `string` | QR code background color (empty blocks).| `white` |
+| `errorCorrection`  | `low | medium | quartile | high` | Error correction level. See [node-qrcode docs](https://github.com/soldair/node-qrcode#error-correction-level) for more details.| `medium` |
+| `outerSpacing`  | `number` | Margin around generated QR code. Will also be used as spacing when saving as image | `0` |
+
+# CyberButton instance methods
+`saveImg` - Async method to generate image out of current QR code and return temp uri.
+
+# React Native Epicode Academy
+This package is a part of [React Native Epicode Academy](https://www.youtube.com/channel/UCjeodJlopQe4b91Y-uS18Ow) YouTube series.
+
---
+++ index.js
@@ -0,0 +1,111 @@
+import React, {
+  useEffect,
+  useState,
+  useRef,
+  useCallback,
+  forwardRef,
+  useImperativeHandle,
+} from 'react';
+import {StyleSheet, View} from 'react-native';
+import QRCodeGenerator from 'qrcode';
+
+let ViewShot = null;
+try {
+  ViewShot = require('react-native-view-shot').default;
+} catch (e) {}
+
+function QRCode(
+  {
+    size = 200,
+    value,
+    color = 'black',
+    backgroundColor = 'white',
+    errorCorrection = 'medium',
+    outerSpacing = 0,
+  },
+  ref,
+) {
+  const [qrData, setQrData] = useState(null);
+  const qrViewRef = useRef();
+
+  const generateQR = useCallback(async (text, errorCorrectionLevel) => {
+    try {
+      const res = await QRCodeGenerator.create(text, {errorCorrectionLevel});
+      setQrData(res.modules);
+    } catch (err) {
+      console.error(err);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (value) {
+      generateQR(value, errorCorrection);
+    } else {
+      setQrData(null);
+    }
+  }, [errorCorrection, generateQR, value]);
+
+  const saveImg = async () => {
+    if (!ViewShot) {
+      console.warn(
+        'react-native-view-shot library not installed. Cannot generate image',
+      );
+    }
+    try {
+      const uri = await qrViewRef.current.capture();
+      return uri;
+    } catch (e) {
+      console.warn(e);
+    }
+  };
+
+  useImperativeHandle(ref, () => ({
+    saveImg,
+  }));
+
+  if (!qrData) {
+    return null;
+  }
+
+  const rows = getRows(Array.from(qrData.data), qrData.size);
+  const blockSize = Math.floor(size / qrData.size);
+
+  return (
+    <ViewShot ref={qrViewRef}>
+      <View style={{margin: outerSpacing}}>
+        {rows.map((row, rowIndex) => (
+          <View key={rowIndex} style={styles.row}>
+            {row.map((block, blockIndex) => (
+              <View
+                key={blockIndex}
+                style={[
+                  {
+                    width: blockSize,
+                    height: blockSize,
+                    backgroundColor: block ? color : backgroundColor,
+                  },
+                ]}
+              />
+            ))}
+          </View>
+        ))}
+      </View>
+    </ViewShot>
+  );
+}
+
+function getRows(array, rowSize) {
+  let rows = [];
+  for (let i = 0; i < array.length; i += rowSize) {
+    rows.push(array.slice(i, i + rowSize));
+  }
+  return rows;
+}
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+  },
+});
+
+export default forwardRef(QRCode);
